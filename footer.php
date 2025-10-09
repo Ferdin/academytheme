2f225f8de291d6a97853b176e86345a3d2e9a8e8
Ferdin <ferdnorbert@gmail.com>
2025-10-08 22:58:31 -0400

    test on footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
     </main>
     <footer class="site-footer">
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); // test?></p>
     </footer>
     <?php wp_footer();?>
 </body>
